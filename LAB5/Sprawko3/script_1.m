6b944750b130a74903f2e8123518dc9e7d359ec5
John15321 <56229312+John15321@users.noreply.github.com>
2019-11-10 14:55:48 +0100

    jprdl

--- LAB5/Sprawko3/script_1.m
+++ LAB5/Sprawko3/script_1.m
@@ -2,6 +2,7 @@
 %x=A*exp((a_0)/(a_1))+((b*u)/(a_0))
 clear;
 close all;
+
 a_0=1;
 a_1=1;
 b=2;
@@ -10,13 +11,22 @@ b=2;
 u=1;
 %przesuniedie poczatku robienia wykresu
 t0=1;
-%Wartosc poczatkowa
+%wart pocz
 x0=0;
-%Amplituda
-du=1;
 
+%111111111111111111111111111111111111111111111111111
 [t]=sim('model_1');
-
-
+subplot(2,1,1);
 plot(t,x);
+hold on;
+title('x(0)=0');
+grid on;
+
+
+t0=0;
+x0=2;
+[t]=sim('model_1');
+subplot(2,1,2);
+plot(t,x);
+title('x(0)=2');
 grid on;
