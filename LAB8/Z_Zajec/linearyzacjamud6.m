a726e5c3ec1823eb46feee54681559e1f7be52fa
John15321 <zakuzoani@gmail.com>
2019-11-26 16:30:11 +0100

    asd

--- LAB8/Z_Zajec/linearyzacjamud6.m
+++ LAB8/Z_Zajec/linearyzacjamud6.m
@@ -12,22 +12,31 @@ a1 = Aw1*sqrt(2*g);
 a2 = Aw2*sqrt(2*g);
 
 fwemax = a1*h1max;
-fwe0 = 0.1*fwemax;
-fwe = 0.7*fwemax;
+fwe0 = 0.5*fwemax;
+fwe = 0.9*fwemax;
 
 h1pocz = fwe0/a1;
 h2pocz = fwe0/a2;
 
-figure(1);
-[t] = sim('kaskadydolinearyzacjimud6.slx');
-plot(t.tout,t.h1,'r');
-hold on;
-plot(t.tout,t.h2,'b');
-xlim([0 150]);
-ylim([0 10]);
-hold off;
-xlabel('czas [s]');
-ylabel('h(t)');
-legend('zale¿noœæ wysokoœci w pierwszym zbiorniku od czasu','zale¿noœæ wysokoœci w drugim zbiorniku od czasu');
-title('Wykres rozwi¹zania równania ró¿niczkowego dla modelu zlinearyzowanego, gdy fwe = 0');
+A=[-(a1/A1), 0; a1*A1, -(a2/A2)];
+B=[1/A1; 0];
+C=[1,0;0,1];
+D=[0;0];
+IC=[h1pocz;h2pocz];
+
+
+
+% 
+% figure(1);
+% [t] = sim('statespace.slx');
+% plot(t.tout,t.h1,'r');
+% hold on;
+% plot(t.tout,t.h2,'b');
+% xlim([0 150]);
+% ylim([0 10]);
+% hold off;
+% xlabel('czas [s]');
+% ylabel('h(t)');
+% legend('zale¿noœæ wysokoœci w pierwszym zbiorniku od czasu','zale¿noœæ wysokoœci w drugim zbiorniku od czasu');
+% title('Wykres rozwi¹zania równania ró¿niczkowego dla modelu zlinearyzowanego, gdy fwe = 0');
 
