c6b62e15b1f054bd3bd2deb716c3651430c5b2b5
John15321 <56229312+John15321@users.noreply.github.com>
2019-12-05 20:15:43 +0100

    :(

--- LAB6/Sprawko4/Skrypt/ksi05.m
+++ LAB6/Sprawko4/Skrypt/ksi05.m
@@ -30,13 +30,15 @@ ksi_colors=["bo","mo","go","ro"]
 
 hold on;
 grid on;
+
 for i=1:1:(length(ksi_all))
     lambda1 = -ksi_all(i)*w+w*sqrt(ksi_all(i)*ksi_all(i)-1);
     lambda2 = -ksi_all(i)*w-w*sqrt(ksi_all(i)*ksi_all(i)-1);
     
     plot(real(lambda1),imag(lambda1),ksi_colors(i));
     plot(real(lambda2),imag(lambda2),ksi_colors(i));
-    
+
+
     line([0,(2*real(lambda1))],[0,0],'Color','k');
     line([0,(2*real(lambda2))],[0,0],'Color','k');
     
