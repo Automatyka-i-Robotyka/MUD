2834acdc9f5dbbd0dcaa50c9efe25c33fcf7af1e
John15321 <zakuzoani@gmail.com>
2019-12-10 15:18:50 +0100

    .

--- LAB9_Start_MiniProjekt/dom_script.m
+++ LAB9_Start_MiniProjekt/dom_script.m
@@ -9,25 +9,20 @@ h=3;%m
 a=6;%m
 V_gp=(a^2)*h;%m^3
 V_pp=(1/3)*V_gp;%m^3
-
 C_vw=c_p*ro_p*V_gp;
 C_vp=c_p*ro_p*V_pp;
-
-K_1=50;
-K_w=50;
-K_p=5;
-
+% K_1=50;
+% K_w=50;
+% K_p=5;
 T_wew_0=21;
 T_p_0=18;
-
-
-
-
-
-
-
-
-
-
-
-
+% T_wew_st = (c_p*ro_p*f_pn
+% liczba = 4; %proporcja kp do kw
+% A = [T_wew-T_pn, T_wew-T_zewn;
+%     T_wew-T_pn, -(T_pn-T_zewn)/liczba];
+% B = [c_p*ro_p*f_pn*(T_zn-T_wew); 0];
+% 
+% x = inv(A)*B
+% K_1=x(1,1);
+% K_w=x(2,1);
+% K_p=(1/liczba)*K_w;
