d630bb7b4bf700ba081c02759195728377ebee8d
Marcin Gruchała <margruchala@gmail.com>
2019-12-16 09:54:42 +0100

    Poprawione

--- mud ostatnie sprawko/nieliniowy.m
+++ mud ostatnie sprawko/nieliniowy.m
@@ -1,22 +1,23 @@
-%model nieliniowy
+ %model nieliniowy
 clear all;
 g=9.81;
 A1=4;
 A2=4;
-Aw1=4;
-Aw2=4;
+Aw1=0.5;
+Aw2=0.5;
 h_max=6;
 fwe_max=Aw1*sqrt(2*g*h_max);
 fwe=0.9*fwe_max;
 dfwe=0.1*fwe_max;
 h10=(fwe^2)/((Aw1^2)*2*g);
 h20=(fwe^2)/((Aw2^2)*2*g);
-[t]=sim('schemat_nieliniowy');
+czas=80;
+[t]=sim('schemat_nieliniowy',czas);
 hold on;
 plot(t,h1,'b');
 plot(t,h2,'r');
 legend('h1','h2');
 xlabel('t');
 ylabel('h');
-title('odpowiedü skokowa dla modelu nieliniwego, fwe=0,9*fwe_{max}');
+title('odpowiedü skokowa dla modelu nieliniwego, fwe=0.9*fwe_{max}');
 
