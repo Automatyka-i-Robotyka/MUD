5411336413679f84d230e1f6229c57da98ee457d
John15321 <56229312+John15321@users.noreply.github.com>
2020-01-05 02:25:25 +0100

    Z

--- LAB9_Start_MiniProjekt/Skrypty/my_dom.m
+++ LAB9_Start_MiniProjekt/Skrypty/my_dom.m
@@ -2,55 +2,132 @@ clear;
 close all
 % Jan Bronicki 249011
 
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % NOMINALNE WARTOSCI
-Tzew = -1;  % 'C
-Tz = 26;    % 'C
-Tp = 18;    % 'C
-Twew = 24;  % 'C
-f_p = 1;     % m^3/s
-%%%%%%%%%%%%
-cp = 1000; % J/(kg*K)
+T_zewN = 23;  % 'C
+T_zN = 24;    % 'C
+T_pN = 19;    % 'C
+T_wewN = 21;  % 'C
+f_pN = 1;     % m^3/s
+c_p = 1000; % J/(kg*K)
 ro_p = 1.2; % kg/m^3
 dl = 20;   % m
 szer = 10; % m
 h_w = 5;   % m
 h_p = 1.5; % m
-Vw = dl*szer*h_w;   % m
-Vp = dl*szer*h_p/3; % m dach jest ostroslupem
-Cvw = cp*ro_p*Vw;    % J/K
-Cvp = cp*ro_p*Vp;    % J/k
-
+V_w = dl*szer*h_w;   % m
+V_p = dl*szer*h_p/3; % m dach jest ostroslupem
+C_vw = c_p*ro_p*V_w;   % J/K
+C_vp = c_p*ro_p*V_p;   % J/k
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Do zmniejszenia zapisu
-a=cp*ro_p*f_p;
+a=c_p*ro_p*f_pN;
 % Proporcja K
 p=0.25;
-
 % Obliczanie wspolczynnikow K
-
-A = [Twew-Tp, Twew-Tzew; 
-     Twew-Tp, -p*(Tp-Tzew)];
-B = [a*(Tz-Twew); 0];
+A = [T_wewN-T_pN, T_wewN-T_zewN; 
+     T_wewN-T_pN, -p*(T_pN-T_zewN)];
+B = [a*(T_zN-T_wewN); 0];
 K_matrix = inv(A)*B;
-
 % Wspolczynniki przenikalnosci cieplnej K
-K_1=K_matrix(1,1);
-K_w=K_matrix(2,1);
-K_p=p*K_w;
-
+K_1=K_matrix(1,1); % W/K
+K_w=K_matrix(2,1); % W/K
+K_p=p*K_w;         % W/K
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Sprawdzenie "Prostej kreski"
-
 % Warunki poczatkowe
-Tp0=Tp;
-Twew0=Twew;
 
+% Wartosci skoków w step'ach
+% Aby otrzymac prosta kreske skoki =0
+steptime=1000;
+d_T_z = 1;
+d_f_p = 0;
+d_T_zew = 0;
 sim('my_dom_model');
 
-
-plot(t,Twew_sym1)
+% Test prostej kreski
+plot(t,T_wew_sym1)
 hold on;
-plot(t,Tp_sym1)
+plot(t,T_p_sym1)
 grid on;
+title('Test prostej kreski')
+xlabel('Czas [s]')
+ylabel("Temperatura [^{\circ}C]")
+legend('T_{wew}','T_{p}')
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Badanie modelu skokami
+% Tz
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
