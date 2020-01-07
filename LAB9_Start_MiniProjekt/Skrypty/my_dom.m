1831c35d4e82aea90b5e4128b33dc4e01ffaf8ee
John15321 <56229312+John15321@users.noreply.github.com>
2020-01-07 01:18:17 +0100

    .

--- LAB9_Start_MiniProjekt/Skrypty/my_dom.m
+++ LAB9_Start_MiniProjekt/Skrypty/my_dom.m
@@ -1,4 +1,3 @@
-%%
 clear;
 close all
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -35,7 +34,8 @@ K_p=p*K_w;         % W/K
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Sprawdzenie "Prostej kreski"
 % Warunki poczatkowe
-
+T_wew0 = T_wewN
+T_p0=T_pN
 % Wartosci skoków w step'ach
 % Aby otrzymac prosta kreske skoki =0
 steptime=1000;
@@ -45,6 +45,7 @@ d_T_zew = 0;
 sim('my_dom_model');
 
 % Test prostej kreski
+figure
 plot(t,T_wew_sym1)
 hold on;
 plot(t,T_p_sym1)
@@ -53,39 +54,179 @@ title('Test prostej kreski')
 xlabel('Czas [s]')
 ylabel("Temperatura [^{\circ}C]")
 legend('T_{wew}','T_{p}')
+
+
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%
-% NOMINALNIE
+figure
+% OGOLNE POTWIERDZENIE MODELI
 % Badanie modelu skokami
 % T_zN
-d_T_z = 0;
+d_T_z = 5;
 d_f_p = 0;
-d_T_zew = 5;
+d_T_zew = 0;
 sim('my_dom_model');
-figure
+
+subplot(3,1,1)
 plot(t,T_wew_sym1)
 hold on;
 plot(t,T_p_sym1)
 grid on;
-title('Skok T_{zew}')
+title('POTWIERDZENIE MODELI, Skok T_{z}')
 xlabel('Czas [s]')
 ylabel("Temperatura [^{\circ}C]")
 legend('T_{wew}','T_{p}')
 
+subplot(3,1,2)
+% T_zewN
+d_T_z = 0;
+d_f_p = 0;
+d_T_zew = 5;
+sim('my_dom_model');
+plot(t,T_wew_sym1)
+hold on;
+plot(t,T_p_sym1)
+grid on;
+xlabel('Czas [s]')
+ylabel("Temperatura [^{\circ}C]")
+legend('T_{wew}','T_{p}')
+title('Skok T_{zewN}')
 
-%%
+subplot(3,1,3)
+% T_zewN
+d_T_z = 0;
+d_f_p = f_pN*0.5;
+d_T_zew = 0;
+sim('my_dom_model');
+plot(t,T_wew_sym1)
+hold on;
+plot(t,T_p_sym1)
+grid on;
+xlabel('Czas [s]')
+ylabel("Temperatura [^{\circ}C]")
+legend('T_{wew}','T_{p}')
+title('Skok f_{pN}')
+hold on
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% NOMINALNE NOMINALNE NOMINALNE NOMINALNE NOMINALNE NOMINALNE
+% dla nominalnego pozostaja te same
+figure
+%-----------------------------------
+% T_z
+subplot(3,2,1)
+d_T_z = 5;
+d_T_zew = 0;
+d_f_p = 0;
+sim('my_dom_model');
+plot(t,T_wew_sym1)
+xlabel('Czas [s]')
+ylabel("T_{wew} [^{\circ}C]")
+hold on;
+grid on
+title('Skok dT_{z}=5')
+
+subplot(3,2,2)
+plot(t,T_p_sym1)
+grid on;
+xlabel('Czas [s]')
+ylabel("T_{p} [^{\circ}C]")
+% legend('T_{wew}','T_{p}')
+title('Skok dT_{z}=5')
+hold on
+%-----------------------------------
+% T_zew
+subplot(3,2,3)
+d_T_z = 0;
+d_T_zew = 2;
+d_f_p = 0;
+sim('my_dom_model');
+plot(t,T_wew_sym1)
+xlabel('Czas [s]')
+ylabel("T_{wew} [^{\circ}C]")
+hold on;
+grid on
+title('Skok dT_{zew}=2')
+hold on
+
+subplot(3,2,4)
+plot(t,T_p_sym1)
+grid on;
+xlabel('Czas [s]')
+ylabel("T_{p} [^{\circ}C]")
+% legend('T_{wew}','T_{p}')
+title('Skok dT_{zew}=2')
+%-----------------------------------
+% f_p
+subplot(3,2,5)
+d_T_z = 0;
+d_T_zew = 0;
+d_f_p = 0.2;
+sim('my_dom_model');
+plot(t,T_wew_sym1)
+xlabel('Czas [s]')
+ylabel("T_{wew} [^{\circ}C]")
+hold on;
+grid on
+title('Skok df_{p}=0.2')
+
+subplot(3,2,6)
+plot(t,T_p_sym1)
+grid on;
+xlabel('Czas [s]')
+ylabel("T_{p} [^{\circ}C]")
+% legend('T_{wew}','T_{p}')
+title('Skok df_{p}=0.2')
+hold on
+%-----------------------------------
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Zmienione T_zew i T_z
+% figure
+
+T_z0 = 30;      % Nowe T_z
+T_zew0 = -8;    % Nowe T_zew
+f_p0 = f_pN;    % Nowe f_p
+% x = (Tp0, Twew0)
+A = [-K_1, c_p*ro_p*f_p0+K_1+K_w;
+    K_1+K_p, -K_1];
+B = [c_p*ro_p*f_p0*T_z0+T_zew0*K_w; K_p*T_zew0];
+x = inv(A)*B;
+T_p0 = x(1,1)
+T_wew0 = x(2,1)
+%-----------------------------------
+% T_z
+subplot(3,2,1)
+d_T_z = 5;
+d_T_zew = 0;
+d_f_p = 0;
+sim('my_dom_model');
+plot(t,T_wew_sym1)
+xlabel('Czas [s]')
+ylabel("T_{wew} [^{\circ}C]")
+hold on;
+grid on
+title('Skok dT_{z}=5')
+
+subplot(3,2,2)
+plot(t,T_p_sym1)
+grid on;
+xlabel('Czas [s]')
+ylabel("T_{p} [^{\circ}C]")
+% legend('T_{wew}','T_{p}')
+title('Skok dT_{z}=5')
+hold on;
 
 
 
 
-Ok czyli 3 razy robimy badania:
-1.Nominalne
-2.zmienione TZew i Tz
-3. Zmienione TZew Tz i fp
 
-Za kazdym razem robimy 3 skoki:
-dTzew= (jakies np:) 5
-dTz=np:5
-dfp=np: fpN*0.5
+% Ok czyli 3 razy robimy badania:
+% 1.Nominalne
+% 2.zmienione TZew i Tz
+% 3. Zmienione TZew Tz i fp
+% 
+% Za kazdym razem robimy 3 skoki:
+% dTzew= (jakies np:) 5
+% dTz=np:5
+% dfp=np: fpN*0.5
 
 
